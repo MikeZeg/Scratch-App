413bca07e824fa81654f5c4f9369ae519d932502
MikeZeg <michalzegzula@gmail.com>
2024-06-04 19:31:57 +0100

    add style to landing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -8,37 +8,40 @@ const LandingPage = () => {
 
     return (
         <div>
-            <header>
+            <header className="header">
             {/* logo */}
                 <div className="logo">
-                    <img src="" alt="company logo" className="logo__img"/>
+                    <img src="" alt="company logo" className="header__logo__img"/>
                 </div>
             {/* navigation */}
-                <div>
-                    <button>Log In</button>
-                    <button>Sign Up</button>
+                <div className="nav">
+                    <button className="btn nav__btn">Log In</button>
+                    <button className="btn nav__btn">Sign Up</button>
                 </div>
             </header>
 
             {/* Content */}
-            <main>
+            <main className="main">
                 <div className="container">
-                    <h1 className="title">Scratch App</h1>
-                    <h2 className="subtitle">Discover the Thrill of Instant Winnings with the Scratch App
-                    <p>Instantly see your chances of winning big prizes in UK scratch games</p>
-                    <p>Comprehensive database of scratch card details and odds</p>
-                    <p>Personalized recommendations to maximize your winnings</p>
+                    <h1 className="title h1">Scratch App</h1>
+                    <h2 className="subtitle h2">Discover the Thrill of Instant Winnings with the Scratch App</h2>
+                    
+                    <div className="list">
+                        <p className="p">Instantly see your chances of winning big prizes in UK scratch games</p>
+                        <p className="p">Comprehensive database of scratch card details and odds</p>
+                        <p className="p">Personalized recommendations to maximize your winnings</p>
+                    </div>
 
-                    <p>The Scratch App is your key to unlocking the excitement of instant win scratch cards in the UK. 
+                    <p className="p content">The Scratch App is your key to unlocking the excitement of instant win scratch cards in the UK. 
                     With a vast database of game details and odds, you'll know exactly what your chances are of hitting the jackpot before you even scratch. 
                     Get personalized recommendations on the best cards to play and make the most of every pound spent. 
-                    Whether you're a seasoned scratcher or new to the thrill, the Scratch App puts the power to win big right at your fingertips.</p></h2>
+                    Whether you're a seasoned scratcher or new to the thrill, the Scratch App puts the power to win big right at your fingertips.</p>
                 </div>
             </main>
             
             {/* Footer */}
                 <footer className="footer">
-                    <p>Handcraf By <a href="https://www.github.com/MikeZeg" target="_blank" className="handcraft">MikeZeg</a> ©2024</p>
+                    <p className="p footer__p">Handcraf By <a href="https://www.github.com/MikeZeg" target="_blank" className="handcraft a-link">MikeZeg</a> ©2024</p>
                 </footer>
         </div>
     );
