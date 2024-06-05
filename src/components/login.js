20eae991ab75be5a34fe22fac41088cefb5c2cac
MikeZeg <michalzegzula@gmail.com>
2024-06-05 15:38:37 +0100

    add firebase config

---
+++ src/components/login.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDom from "react-dom";
+import {} from "../config/firebase.js";
+import "../styles/loginStyle.css";
---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD58Z-yazoD6td3Otno1zADsHi_ykibELE",
+  authDomain: "scratch-app-1eb1b.firebaseapp.com",
+  projectId: "scratch-app-1eb1b",
+  storageBucket: "scratch-app-1eb1b.appspot.com",
+  messagingSenderId: "1088403149631",
+  appId: "1:1088403149631:web:2cfa6a16c888d1322d798a",
+  measurementId: "G-1N6XCB9HMF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
