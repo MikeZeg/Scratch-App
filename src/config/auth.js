eb4d27d211e989684aaeeb006f0a6a5cc07c0fba
MikeZeg <michalzegzula@gmail.com>
2024-06-07 17:41:29 +0100

    struct of login methods

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,3 +1,20 @@
-export const Auth = async () => {
- 
-}
+import React, { useState } from "react";
+import ReactDom from "react-dom";
+import { auth } from "../config/firebase.js";
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+
+
+    export const Auth = async () => {
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+
+        const signIn = async () => {
+            try {
+                await createUserWithEmailAndPassword(auth, email, password)
+            }catch (err){
+                console.error(err)
+            }
+    }
+    
+    };
