3d21c73c51e55ed789442b2914055feb84dd82a0
MikeZeg <michalzegzula@gmail.com>
2025-01-04 22:35:22 +0000

    add style to new component

--- src/components/mainComponents/ScratchStats.js
+++ src/components/mainComponents/ScratchStats.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate} from "react-router-dom";
-import '../../styles/scratchStatsStyle.css'
+import '../../styles/scratchStatsStyle.css';
+import '../../styles/mainStyle.css';
+import '../../styles/navBarStyle.css';
 import { NavBar } from './NavBar';
 
 
@@ -8,14 +10,19 @@ export const ScratchStats = () => {
     const goTo = useNavigate();
 
     return (
-        <>
-            <NavBar/>
-            <div className='scratchStats'>  
+        <div className='scratchStats'>
+
+            <div className='scratchStats__navBar'>
+                <NavBar/>
+            </div>
+
+            <div className='scratchStats__content'>  
                 <section > Check scratch Cards </section>
                 <section > Give diagram winning ratio - all users</section>
                 <section > Give user diagram winning ration - select data? </section>
                 <button className='btn rounded' onClick={()=>{goTo('/Main')}}> Go To Main</button>
             </div>
-        </>
+
+        </div>
     )
 }
